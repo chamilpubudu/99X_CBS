2e05ccb9f4bd8a51f3711653255fd820597dd9ca
mal90 <madkonline@gmail.com>
2014-10-15 16:11:42 +0530

    System settings saving part changes

--- Areas/Admin/Controllers/SettingsController.cs
+++ Areas/Admin/Controllers/SettingsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _99X_CBS.Models;
+
 
 namespace _99X_CBS.Areas.Admin.Controllers
 {
     public class SettingsController : Controller
     {
+        Entities db = new Entities();
         //
         // GET: /Admin/Settings/
         public ActionResult Index()
@@ -16,15 +19,54 @@ namespace _99X_CBS.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public ActionResult UploadPhoto(String systemName,HttpPostedFileBase photo )
+        public ActionResult UploadPhoto(String systemName, HttpPostedFileBase photo)
         {
-           
 
-            if (photo != null) {
+
+            if (photo != null)
+            {
                 string path = HttpContext.Server.MapPath(@"\Content\Images\" + photo.FileName);
                 photo.SaveAs(path);
+
+                List<CBS_SystemSettings> CBS_PhotoNameList = db.CBS_SystemSettings.Where(x => x.Name == "Photo Name").ToList();
+
+                if (CBS_PhotoNameList.Count > 0)
+                {
+                    CBS_SystemSettings cbs_systemsettings2 = CBS_PhotoNameList.ElementAt(0);
+                    cbs_systemsettings2.Value = photo.FileName;
+                    db.SaveChanges();
+                }
+                else
+                {
+
+                    CBS_SystemSettings cbs_systemsettings2 = new CBS_SystemSettings();
+                    cbs_systemsettings2.Name = "Photo Name";
+                    cbs_systemsettings2.Value = photo.FileName;
+                    db.CBS_SystemSettings.Add(cbs_systemsettings2);
+                    db.SaveChanges();
+
+                }
+            }
+            List<CBS_SystemSettings> CBS_SystemNameList = db.CBS_SystemSettings.Where(x => x.Name == "System Name").ToList();
+
+            if (CBS_SystemNameList.Count > 0)
+            {
+                CBS_SystemSettings cbs_systemsettings1 = CBS_SystemNameList.ElementAt(0);
+                cbs_systemsettings1.Value = systemName;
+                db.SaveChanges();
+
+            }
+            else
+            {
+
+                CBS_SystemSettings cbs_systemsettings1 = new CBS_SystemSettings();
+                cbs_systemsettings1.Name = "System Name";
+                cbs_systemsettings1.Value = systemName;
+                db.CBS_SystemSettings.Add(cbs_systemsettings1);
+                db.SaveChanges();
+
             }
             return RedirectToAction("Index", "Settings", new { area = "Admin" });
         }
-	}
+    }
 }
---
+++ Areas/CBS/Models/CBS_SystemSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _99X_CBS.Models
+{
+    public class CBS_SystemSettings
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public int ID { get; set; }
+    }
+}
