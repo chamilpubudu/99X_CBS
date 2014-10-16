3b18e121f7ff487d1abb3fec5715ca088030a50f
mal90 <madkonline@gmail.com>
2014-10-16 14:48:00 +0530

    Bug fixes for system settings functionality

--- Models/SystemSettings.cs
+++ Models/SystemSettings.cs
@@ -13,8 +13,15 @@ namespace _99X_CBS.Models
             string _GlobalAppName = (string)HttpContext.Current.Application["_GlobalAppName"];
             if (_GlobalAppName != null || _GlobalAppName != "")
             {
-                CBS_SystemSettings cbs_systemsettings = db.CBS_SystemSettings.Where(x => x.Name == "System Name").First();
-                _GlobalAppName = cbs_systemsettings.Value;
+                try
+                {
+                    CBS_SystemSettings cbs_systemsettings = db.CBS_SystemSettings.Where(x => x.Name == "System Name").First();
+                    _GlobalAppName = cbs_systemsettings.Value;
+                }
+                catch (InvalidOperationException e)
+                {
+                    _GlobalAppName = "CBS";
+                }
                 
             }
             
@@ -27,8 +34,15 @@ namespace _99X_CBS.Models
             string _GlobalAppLogo = (string)HttpContext.Current.Application["_GlobalAppLogo"];
             if (_GlobalAppLogo != null || _GlobalAppLogo != "")
             {
-                CBS_SystemSettings cbs_systemsettings = db.CBS_SystemSettings.Where(x => x.Name == "Photo Name").First();
-                _GlobalAppLogo = cbs_systemsettings.Value;
+                try
+                { 
+                    CBS_SystemSettings cbs_systemsettings = db.CBS_SystemSettings.Where(x => x.Name == "Photo Name").First();
+                    _GlobalAppLogo = cbs_systemsettings.Value;
+                }
+                catch (InvalidOperationException e)
+                {
+                    _GlobalAppLogo = "logo-black.png";
+                }
 
             }
 
