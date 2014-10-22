42f60e19fb5622d161ad5fc2f370cba3718c3264
mal90 <madkonline@gmail.com>
2014-10-22 15:12:10 +0530

    Notification Model Added

---
+++ Models/CBS_NotificationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _99X_CBS.Models
+{
+    public class CBS_NotificationInfo
+    {
+        public string NotificationID { get; set; }
+        public bool Viewed { get; set; }
+        public Nullable<System.DateTime> NotifiedTime { get; set; }
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public string UserID { get; set; }
+        public string Link { get; set; }
+    }
+}
--- Models/Entities.cs
+++ Models/Entities.cs
@@ -30,5 +30,6 @@ namespace _99X_CBS.Models
         public DbSet<CBS_UniversitySessions> CBS_UniversitySessions { get; set; }
         public DbSet<CBS_ValueInnovations> CBS_ValueInnovations { get; set; }
         public DbSet<CBS_SystemSettings> CBS_SystemSettings { get; set; }
+        public DbSet<CBS_NotificationInfo> CBS_NotificationInfo { get; set; }
     }
 }
